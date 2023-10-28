c969db176f38c134f80748aeed7ba6c5789e919e
Aleksandar Ristic <storiaca@gmail.com>
2023-10-28 12:55:43 +0200

    Working with object types

--- 02-section-ts-basic/first-app.js
+++ 02-section-ts-basic/first-app.js
@@ -6,3 +6,12 @@ var isValid = true; // boolean type
 /* Union types */
 var userId = "123abc";
 userId = 123;
+/* Object types */
+// let user: object; // not best practice
+var user;
+user = {
+    name: "Max",
+    age: 34,
+    isAdmin: true,
+    id: "abc", // 123
+};
--- 02-section-ts-basic/first-app.ts
+++ 02-section-ts-basic/first-app.ts
@@ -10,3 +10,19 @@ let isValid = true; // boolean type
 let userId: string | number = "123abc";
 
 userId = 123;
+
+/* Object types */
+// let user: object; // not best practice
+let user: {
+  name: string;
+  age: number;
+  isAdmin: boolean;
+  id: string | number;
+};
+
+user = {
+  name: "Max",
+  age: 34,
+  isAdmin: true,
+  id: "abc", // 123
+};
