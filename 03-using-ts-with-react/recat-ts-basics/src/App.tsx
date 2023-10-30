b2f3469386b39fad341f4ced97d8ec75d066cb12
Aleksandar Ristic <storiaca@gmail.com>
2023-10-30 16:02:05 +0100

    Defining component props types

--- 03-using-ts-with-react/recat-ts-basics/src/App.tsx
+++ 03-using-ts-with-react/recat-ts-basics/src/App.tsx
@@ -2,8 +2,10 @@ import CourseGoal from "./components/CourseGoal";
 export default function App() {
   return (
     <main>
-      <h1>Let's get started!</h1>
-      <CourseGoal />
+      <CourseGoal
+        title="Learn React + TS"
+        description="Learn it from the ground up"
+      />
     </main>
   );
 }
--- 03-using-ts-with-react/recat-ts-basics/src/components/CourseGoal.tsx
+++ 03-using-ts-with-react/recat-ts-basics/src/components/CourseGoal.tsx
@@ -1,9 +1,15 @@
-const CourseGoal = () => {
+const CourseGoal = ({
+  title,
+  description,
+}: {
+  title: string;
+  description: string;
+}) => {
   return (
     <article>
       <div>
-        <h2>Title</h2>
-        <p>Description</p>
+        <h2>{title}</h2>
+        <p>{description}</p>
       </div>
       <button>Delete</button>
     </article>
