629c1b909432038024dfb9f5f0da8a6c613732df
Aleksandar Ristic <storiaca@gmail.com>
2023-10-30 15:50:00 +0100

    Building a first component

---
+++ 03-using-ts-with-react/recat-ts-basics/src/components/CourseGoal.tsx
@@ -0,0 +1,13 @@
+const CourseGoal = () => {
+  return (
+    <article>
+      <div>
+        <h2>Title</h2>
+        <p>Description</p>
+      </div>
+      <button>Delete</button>
+    </article>
+  );
+};
+
+export default CourseGoal;
