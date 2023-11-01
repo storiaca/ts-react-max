98e566d9083f1e5b4ea81b12cae55da4fd1994d3
Aleksandar Ristic <storiaca@gmail.com>
2023-11-01 13:35:53 +0100

    Passing functions as values in a type-safe wat

--- 03-using-ts-with-react/react-ts-basics/src/components/CourseGoal.tsx
+++ 03-using-ts-with-react/react-ts-basics/src/components/CourseGoal.tsx
@@ -5,16 +5,20 @@ import { FC, type PropsWithChildren, type ReactNode } from "react";
 //   children: ReactNode;
 // }
 
-type CourseGoalProps = PropsWithChildren<{ title: string }>;
+type CourseGoalProps = PropsWithChildren<{
+  id: number;
+  title: string;
+  onDelete: (id: number) => void;
+}>;
 
-const CourseGoal: FC<CourseGoalProps> = ({ title, children }) => {
+const CourseGoal: FC<CourseGoalProps> = ({ title, id, children, onDelete }) => {
   return (
     <article>
       <div>
         <h2>{title}</h2>
         {children}
       </div>
-      <button>Delete</button>
+      <button onClick={() => onDelete(id)}>Delete</button>
     </article>
   );
 };
--- 03-using-ts-with-react/react-ts-basics/src/components/CourseGoalList.tsx
+++ 03-using-ts-with-react/react-ts-basics/src/components/CourseGoalList.tsx
@@ -3,13 +3,14 @@ import { CourseGoal as CGoal } from "../App";
 
 interface CourseGoalListProps {
   goals?: CGoal[];
+  onDeleteGoal: (id: number) => void;
 }
-function CourseGoalList({ goals }: CourseGoalListProps) {
+function CourseGoalList({ goals, onDeleteGoal }: CourseGoalListProps) {
   return (
     <ul>
       {goals?.map((goal) => (
         <li key={goal.id}>
-          <CourseGoal title={goal.title}>
+          <CourseGoal title={goal.title} id={goal.id} onDelete={onDeleteGoal}>
             <p>{goal.description}</p>
           </CourseGoal>
         </li>
