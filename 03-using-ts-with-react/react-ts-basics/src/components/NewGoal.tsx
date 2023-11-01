5b7e9035de4de34e144192910ebe8fafcc1bb5b8
Aleksandar Ristic <storiaca@gmail.com>
2023-11-01 14:07:28 +0100

    Working with generic event types

--- 03-using-ts-with-react/react-ts-basics/src/components/NewGoal.tsx
+++ 03-using-ts-with-react/react-ts-basics/src/components/NewGoal.tsx
@@ -1,18 +1,20 @@
 import { type FormEvent } from "react";
 
 const NewGoal = () => {
-  function handleSumbit(event: FormEvent) {
+  function handleSumbit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
+
+    new FormData(event.currentTarget);
   }
   return (
     <form onSubmit={handleSumbit}>
       <p>
         <label htmlFor="goal">Your goal</label>
-        <input id="goal" type="text" />
+        <input id="goal" type="text" name="goal" />
       </p>
       <p>
         <label htmlFor="summary">Short summary</label>
-        <input id="summary" type="text" />
+        <input id="summary" type="text" name="summary" />
       </p>
       <p>
         <button>Add goal</button>
