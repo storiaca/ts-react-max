f8f63d919f88c5afdbb64667836cf91f8750af94
Aleksandar Ristic <storiaca@gmail.com>
2023-11-02 09:52:10 +0100

    Building a Basic wrapper component

---
+++ 04-advanced-component-types/new-ts-react/src/components/Input.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+type InputProps = {
+  label: string;
+  id: string;
+};
+
+const Input = ({ label, id }: InputProps) => {
+  return (
+    <p>
+      <label htmlFor={id}>{label}</label>
+      <input id={id} type="text" />
+    </p>
+  );
+};
+
+export default Input;
