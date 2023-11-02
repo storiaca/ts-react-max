416fb1e0f876c454cd6c44c93588c69f013b2de5
Aleksandar Ristic <storiaca@gmail.com>
2023-11-02 10:50:08 +0100

    Building a wrapper component taht renders different elements

--- 04-advanced-component-types/new-ts-react/src/App.tsx
+++ 04-advanced-component-types/new-ts-react/src/App.tsx
@@ -1,10 +1,18 @@
 import Input from "./components/Input";
+import Button from "./components/Button";
 function App() {
   return (
     <main>
-      Let's get started!
       <Input id="name" label="Your name" type="text" />
       <Input id="age" label="Your age" type="number" />
+      <p>
+        <Button el="button">A button</Button>
+      </p>
+      <p>
+        <Button el="anchor" href="https://google.com">
+          A Link
+        </Button>
+      </p>
     </main>
   );
 }
---
+++ 04-advanced-component-types/new-ts-react/src/components/Button.tsx
@@ -0,0 +1,18 @@
+import { type ComponentPropsWithoutRef } from "react";
+
+type ButtonProps = {
+  el: "button";
+} & ComponentPropsWithoutRef<"button">;
+
+type AnchorProps = {
+  el: "anchor";
+} & ComponentPropsWithoutRef<"a">;
+const Button = (props: ButtonProps | AnchorProps) => {
+  // const { el, ...otherProps } = props;
+  if (props.el === "anchor") {
+    return <a className="button" {...props}></a>;
+  }
+  return <button className="button" {...props}></button>;
+};
+
+export default Button;
