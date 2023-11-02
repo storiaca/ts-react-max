26c1d825b86793c18200f692dfb381eedb2674e9
Aleksandar Ristic <storiaca@gmail.com>
2023-11-02 09:17:09 +0100

    Problem Flexible comoponents with required prop combinations

--- 04-advanced-component-types/react-ts-advanced/src/components/InfoBox.tsx
+++ 04-advanced-component-types/react-ts-advanced/src/components/InfoBox.tsx
@@ -2,11 +2,12 @@ import { type ReactNode } from "react";
 
 type InfoBoxProps = {
   mode: "hint" | "warning";
+  severity?: "low" | "medium" | "high";
   children: ReactNode;
 };
 
 // info, warning types component
-const InfoBox = ({ mode, children }: InfoBoxProps) => {
+const InfoBox = ({ mode, severity, children }: InfoBoxProps) => {
   if (mode === "hint") {
     return (
       <aside className="infobox infobox-hint">
@@ -16,7 +17,7 @@ const InfoBox = ({ mode, children }: InfoBoxProps) => {
   }
 
   return (
-    <aside className="infobox infobox-warning warning--medium">
+    <aside className={`infobox infobox-warning warning--${severity}`}>
       <h2>Warning</h2>
       <p>{children}</p>
     </aside>
