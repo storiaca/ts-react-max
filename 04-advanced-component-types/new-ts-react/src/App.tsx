ef115c8aca2d479683763fe1b4f568ece14cdea2
Aleksandar Ristic <storiaca@gmail.com>
2023-11-03 10:07:29 +0100

    Building another wrapper component custom Form component

--- 04-advanced-component-types/new-ts-react/src/App.tsx
+++ 04-advanced-component-types/new-ts-react/src/App.tsx
@@ -1,16 +1,17 @@
 import Input from "./components/Input";
 import Button from "./components/Button";
-import Container from "./components/ Container";
-import { useRef } from "react";
-function App() {
-  const input = useRef<HTMLInputElement>(null);
+import Form from "./components/Form";
 
+function App() {
   return (
     <main>
-      <Container as={Button} onClick={() => {}} type="button">
-        Click Me
-      </Container>
-      <Input type="text" label="Test" id="test" ref={input} />
+      <Form>
+        <Input type="text" label="Name" id="name" />
+        <Input type="age" label="Age" id="age" />
+        <p>
+          <Button>Save</Button>
+        </p>
+      </Form>
     </main>
   );
 }
---
+++ 04-advanced-component-types/new-ts-react/src/components/Form.tsx
@@ -0,0 +1,8 @@
+import React, { type ComponentPropsWithoutRef } from "react";
+
+type FormProps = ComponentPropsWithoutRef<"form">;
+function Form(props: FormProps) {
+  return <form {...props}>{props.children}</form>;
+}
+
+export default Form;
