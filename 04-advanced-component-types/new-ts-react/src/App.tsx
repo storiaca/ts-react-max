b70e206dc9fb3faf5e15aa32a40369dde9057281
Aleksandar Ristic <storiaca@gmail.com>
2023-11-04 10:29:24 +0100

    Exposing component apis with useImperativeHandle with ts

--- 04-advanced-component-types/new-ts-react/src/App.tsx
+++ 04-advanced-component-types/new-ts-react/src/App.tsx
@@ -1,31 +1,19 @@
+import { useRef } from "react";
 import Input from "./components/Input";
 import Button from "./components/Button";
-import Form from "./components/Form";
+import Form, { type FormHandle } from "./components/Form";
 
 function App() {
+  const customForm = useRef<FormHandle>(null);
+
   function handleSave(data: unknown) {
     const extractedData = data as { name: string; age: string };
     console.log(extractedData);
-
-    /* Type guards */
-    // if (
-    //   !data ||
-    //   typeof data !== 'object' ||
-    //   !('name' in data) ||
-    //   !('age' in data)
-    // ) {
-    //     return;
-    // }
-
-    // // at this point, TypeScript knows that data MUST BE an object
-    // // with a name and age property
-    // // otherwise, the previous if statement would have returned
-    // console.log(data);
-    // customForm.current?.clear();
+    customForm.current?.clear();
   }
   return (
     <main>
-      <Form onSave={handleSave}>
+      <Form onSave={handleSave} ref={customForm}>
         <Input type="text" label="Name" id="name" />
         <Input type="age" label="Age" id="age" />
         <p>
