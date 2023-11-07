50f374f1b1cdfb7065291b31832ace68c0e567db
Aleksandar Ristic <storiaca@gmail.com>
2023-11-07 10:19:29 +0100

    Section 6 Side effects, useEffect and data fetching

---
+++ 06-side-effects-data-fetching/src/components/Header.tsx
@@ -0,0 +1,23 @@
+import Button from "./UI/Button.tsx";
+import { useTimersContext } from "../store/timers-context.tsx";
+
+export default function Header() {
+  const timersCtx = useTimersContext();
+
+  // if (timersCtx === null) {
+  //   throw new Error("Something went wrong here");
+  // }
+  return (
+    <header>
+      <h1>ReactTimer</h1>
+
+      <Button
+        onClick={
+          timersCtx.isRunning ? timersCtx.stopTimers : timersCtx.startTimers
+        }
+      >
+        {timersCtx.isRunning ? "Stop" : "Start"} Timers
+      </Button>
+    </header>
+  );
+}
